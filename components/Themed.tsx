29802965caf72599e2eb3210afbc60e410a1b09a
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-07-09 21:07:08 -0400

    created authentication context with api requests and login/register pages

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -1,15 +1,23 @@
-import { Text as DefaultText, View as DefaultView } from "react-native";
+// Default components
+import {
+    Text as DefaultText,
+    View as DefaultView,
+    SafeAreaView as DefaultSafeAreaView,
+    StyleSheet,
+} from "react-native";
 
+// Color imports
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "../hooks/useColorScheme";
 
+// Props
 type ThemeProps = {
     lightColor?: string;
     darkColor?: string;
 };
-
 export type TextProps = ThemeProps & DefaultText["props"];
 export type ViewProps = ThemeProps & DefaultView["props"];
+export type SafeAreaViewProps = ThemeProps & DefaultView["props"];
 
 export function useThemeColor(
     props: { light?: string; dark?: string },
@@ -25,21 +33,64 @@ export function useThemeColor(
     }
 }
 
-export function Text(props: TextProps) {
-    const { style, lightColor, darkColor, ...otherProps } = props;
-    const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+// Define styles for different text types
+const typeStyles = StyleSheet.create({
+    h1: {
+        fontSize: 40,
+        fontFamily: "adelia",
+        lineHeight: "100%",
+    },
+    h2: {
+        fontSize: 28,
+        fontFamily: "louisgeorgecafe",
+    },
+    h3: {
+        fontSize: 24,
+        fontFamily: "louisgeorgecafe",
+    },
+    h4: {
+        fontSize: 20,
+        fontFamily: "louisgeorgecafe",
+    },
+    // Add more styles as needed
+});
 
-    return <DefaultText style={[{ color }, style]} {...otherProps} />;
+// Extend TextProps to include the type prop
+interface ExtendedTextProps extends TextProps {
+    type?: keyof typeof typeStyles;
 }
 
+// Custom components
 export function View(props: ViewProps) {
     const { style, lightColor, darkColor, ...otherProps } = props;
     const backgroundColor = useThemeColor(
         { light: lightColor, dark: darkColor },
         "background"
     );
-
     return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
 
+export function SafeAreaView(props: SafeAreaViewProps) {
+    const { style, lightColor, darkColor, ...otherProps } = props;
+    const backgroundColor = useThemeColor(
+        { light: lightColor, dark: darkColor },
+        "background"
+    );
+    return (
+        <DefaultSafeAreaView
+            style={[{ backgroundColor }, style]}
+            {...otherProps}
+        />
+    );
+}
+
+export function Text(props: ExtendedTextProps) {
+    const { style, lightColor, darkColor, type, ...otherProps } = props;
+    const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+    const textStyle = [type ? typeStyles[type] : {}, { color }, style]; // Combine type style, color, and custom style
+
+    return <DefaultText style={textStyle} {...otherProps} />;
+}
+
 // Add more components as we go
+// PRESSABLE
