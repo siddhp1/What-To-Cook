028c4fa18923837f45dfeb8b8c3d1855e7b7360b
siddhp1 <siddh.patel@outlook.com>
2024-07-10 21:03:24 -0400

    created custom theme context

---
+++ constants/Theme.ts
@@ -0,0 +1,24 @@
+import {
+    DarkTheme as NavigationDarkTheme,
+    DefaultTheme as NavigationDefaultTheme,
+} from "@react-navigation/native";
+
+const LightTheme = {
+    ...NavigationDefaultTheme.colors,
+    c1: "#FFFCF2",
+    c2: "#CCC5B9",
+    c3: "#403D39",
+    c4: "#252422",
+    c5: "#c44536",
+};
+
+const DarkTheme = {
+    ...NavigationDarkTheme.colors,
+    c1: "#252422",
+    c2: "#403D39",
+    c3: "#CCC5B9",
+    c4: "#FFFCF2",
+    c5: "#c44536",
+};
+
+export { LightTheme, DarkTheme };
---
+++ contexts/ThemeContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { LightTheme, DarkTheme } from "@/constants/Theme";
+import { useColorScheme } from "react-native";
+
+// Create a context for themes
+type ThemeContextType = {
+    theme: typeof LightTheme | typeof DarkTheme;
+};
+
+const ThemeContext = createContext<ThemeContextType>({
+    theme: LightTheme,
+});
+
+// Custom Theme Provider component
+export const ThemeProvider = ({ children }: any) => {
+    const colorScheme = useColorScheme();
+
+    // Set theme to current color scheme
+    const [theme, setTheme] = useState(
+        colorScheme === "dark" ? DarkTheme : LightTheme
+    );
+
+    // Update theme if color scheme changes
+    useEffect(() => {
+        setTheme(colorScheme === "dark" ? DarkTheme : LightTheme);
+    }, [colorScheme]);
+
+    return (
+        <ThemeContext.Provider value={{ theme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+// Custom useTheme hook to access the current theme
+export const useTheme = () => useContext(ThemeContext);
