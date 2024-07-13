f74eee6505259a58a9c4abde0843eba0800bb89a
Siddh Patel <47033295+siddhp1@users.noreply.github.com>
2024-07-13 15:36:42 -0400

    created interface for add dish page

--- app/(app)/(home)/add.tsx
+++ app/(app)/(home)/add.tsx
@@ -1,24 +1,286 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import axios from "axios";
 
-// Screen to add a dish
+import {
+    View,
+    SafeAreaView,
+    ScrollView,
+    Text,
+    TextInput,
+    Pressable,
+    Alert,
+    StyleSheet,
+    Dimensions,
+} from "react-native";
+import StarRating from "react-native-star-rating-widget";
+
+import { Image } from "expo-image";
+import * as ImagePicker from "expo-image-picker";
+
+import { useTheme } from "@/contexts/ThemeContext";
+
+// Get api url from the context
+import { API_URL } from "@/contexts/AuthContext";
+
+import { FontAwesome6 } from "@expo/vector-icons";
+
 export default function AddScreen() {
+    // Fields
+    const [name, setName] = useState("test");
+    const [image, setImage] = useState<string | null>(null);
+    const [cuisine, setCuisine] = useState("test");
+    const [rating, setRating] = useState<number>(4);
+    const [timeToMake, setTimeToMake] = useState<number>(4);
+
+    // Api request
+    const createDish = async () => {
+        // Check that fields are filled
+        if (!name || !image || !cuisine || !rating || !timeToMake) {
+            Alert.alert("Error", "Please fill out all fields");
+            return;
+        }
+
+        // Create formdata object
+        let formData = new FormData();
+        formData.append("name", name);
+        // Image here
+        formData.append("cuisine", cuisine);
+        formData.append("rating", rating.toString());
+        formData.append("time_to_make", timeToMake.toString());
+
+        try {
+            // Request here
+        } catch (e) {
+            if (axios.isAxiosError(e) && e.response) {
+                console.log(e.response.data);
+                // Check format of responses
+                return {
+                    error: true,
+                    status: e.response.status,
+                    data: e.response.data || "An error occurred.",
+                };
+            } else {
+                return {
+                    error: true,
+                    status: null,
+                    data: "An unexpected error occurred.",
+                };
+            }
+        }
+    };
+
+    // Ask the user if they want to take a photo or select from camera roll
+    const pickOrTake = async () => {
+        Alert.alert(
+            "Upload Photo",
+            "Choose an option",
+            [
+                {
+                    text: "Take Photo",
+                    onPress: () => takePhoto(),
+                },
+                {
+                    text: "Choose from Library",
+                    onPress: () => pickImage(),
+                },
+                {
+                    text: "Cancel",
+                    style: "cancel",
+                },
+            ],
+            { cancelable: true }
+        );
+    };
+
+    const pickImage = async () => {
+        const permission =
+            await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permission.granted) {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                aspect: [1, 1],
+                quality: 1,
+                selectionLimit: 1,
+                allowsEditing: true,
+            });
+
+            console.log(result); // remove this after
+
+            if (!result.canceled) {
+                setImage(result.assets[0].uri);
+            }
+        }
+        return;
+    };
+
+    const takePhoto = async () => {
+        const permission = await ImagePicker.requestCameraPermissionsAsync();
+
+        if (permission.granted) {
+            let result = await ImagePicker.launchCameraAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                aspect: [1, 1],
+                quality: 1,
+                selectionLimit: 1,
+                allowsEditing: true,
+            });
+
+            console.log(result); // remove this after
+
+            if (!result.canceled) {
+                setImage(result.assets[0].uri);
+            }
+        }
+        return;
+    };
+
+    // Theme
+    const { theme } = useTheme();
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Add Dish Screen</Text>
-        </View>
+        <SafeAreaView style={[styles.container, { backgroundColor: theme.c1 }]}>
+            <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+                <TextInput
+                    placeholder="Name of Dish"
+                    placeholderTextColor={theme.c3}
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                    onChangeText={(text: string) => setName(text)}
+                    style={[
+                        styles.fullInput,
+                        { color: theme.c4, backgroundColor: theme.c2 },
+                    ]}
+                />
+                {/* Image */}
+                {/* Conditionally render image */}
+                {image && (
+                    <Image
+                        style={styles.image}
+                        source={{ uri: image }}
+                        contentFit="contain"
+                    />
+                )}
+                <Pressable
+                    onPress={pickOrTake}
+                    style={[styles.button, { backgroundColor: theme.c2 }]}
+                >
+                    <Text style={[styles.text, { color: theme.c5 }]}>
+                        {/* Change text depending on whether or not there already is an image */}
+                        {image ? "Change Image" : "Add Image"}
+                    </Text>
+                </Pressable>
+
+                <TextInput
+                    placeholder="Cuisine"
+                    placeholderTextColor={theme.c3}
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                    onChangeText={(text: string) => setCuisine(text)}
+                    style={[
+                        styles.fullInput,
+                        { color: theme.c4, backgroundColor: theme.c2 },
+                    ]}
+                />
+
+                <View
+                    style={[
+                        styles.ratingContainer,
+                        { backgroundColor: theme.c2 },
+                    ]}
+                >
+                    <Text style={[styles.text, { color: theme.c4 }]}>
+                        Rating
+                    </Text>
+                    <StarRating rating={rating} onChange={setRating} />
+                </View>
+
+                <View
+                    style={[
+                        styles.ratingContainer,
+                        { backgroundColor: theme.c2 },
+                    ]}
+                >
+                    <Text style={[styles.text, { color: theme.c4 }]}>
+                        Time to Cook
+                    </Text>
+                    <StarRating
+                        rating={timeToMake}
+                        onChange={setTimeToMake}
+                        enableHalfStar={false}
+                        color={theme.c4}
+                        emptyColor={theme.c3}
+                        StarIconComponent={({ color }) => (
+                            <FontAwesome6
+                                name="clock"
+                                size={28}
+                                color={color}
+                            />
+                        )}
+                        style={{ marginTop: 5, marginBottom: 2 }}
+                        starStyle={{ marginHorizontal: 7 }}
+                    />
+                </View>
+
+                <Pressable
+                    onPress={createDish}
+                    style={[
+                        styles.button,
+                        { backgroundColor: theme.c2, marginBottom: "4%" },
+                    ]}
+                >
+                    <Text style={[styles.text, { color: theme.c5 }]}>
+                        Add Dish
+                    </Text>
+                </Pressable>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
+
+const screenWidth = Dimensions.get("window").width;
+const imageSize = screenWidth * 0.8;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
     },
-    title: {
-        color: "#ffffff",
-        fontFamily: "GaMaamli",
+    scrollViewContainer: {
+        alignItems: "center",
+    },
+    ratingContainer: {
+        minWidth: "80%",
+        alignItems: "center",
+        marginTop: "4%",
+        paddingVertical: "3%",
+        paddingHorizontal: "3%",
+        borderRadius: 10,
+    },
+    text: {
+        textAlign: "center",
+        fontFamily: "LouisGeorgeCafe",
         fontSize: 20,
-        fontWeight: "bold",
+    },
+    button: {
+        marginTop: "4%",
+        minWidth: "80%",
+        paddingVertical: "3%",
+        paddingHorizontal: "8%",
+        borderRadius: 10,
+    },
+    image: {
+        marginTop: "4%",
+        width: imageSize,
+        height: imageSize,
+        borderRadius: 10,
+    },
+    fullInput: {
+        marginTop: "4%",
+        minWidth: "80%",
+        paddingVertical: "3%",
+        paddingHorizontal: "3%",
+        borderRadius: 10,
+        fontFamily: "LouisGeorgeCafe",
+        fontSize: 20,
     },
 });
